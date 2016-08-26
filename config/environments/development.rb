944d8bf14220d084b6600e34c5bf359601eb158d
Fernando Cisneros <fernando.saxofone@gmail.com>
2016-08-26 18:40:38 -0300

    setup email prod and dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,19 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.sendgrid.net",
+      :port                 => 587,
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => "plain",
+      :domain               => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', :protocol => 'http' }
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,18 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.sendgrid.net",
+      :port                 => 587,
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => "plain",
+      :domain               => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'ferd-photo-app.herokuapp.com', :protocol => 'https' }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
